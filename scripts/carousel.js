b718c2ca9df7c5b143d45a6e1d414328df5b661a
Paul Fabre <67488418+eFaerys@users.noreply.github.com>
2024-01-14 18:54:12 +0100

    feat: converted images

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -1,28 +1,27 @@
 function rotation(entries, observer) {
+    entries.forEach(entry => {
+        const carouselImages = entry.target.querySelectorAll('.carousel-image');
+        let currentImage = 0;
 
-    let carouselImages = document.querySelectorAll('.carousel-image');
-    let currentImage = 0;
-    
-    if (carouselImages.length > 0) {
-        carouselImages[currentImage].style.display = 'block';
-        
-        setInterval(() => {
-            carouselImages[currentImage].style.display = 'none';
-            currentImage++;
-            if (currentImage >= carouselImages.length) {
-                currentImage = 0;
-            }
+        if (carouselImages.length > 0) {
             carouselImages[currentImage].style.display = 'block';
-        }, 5000);
-    } else {
-        console.error("Aucune image de classe 'carousel-image' trouvée.");
-    }
+
+            setInterval(() => {
+                carouselImages[currentImage].style.display = 'none';
+                currentImage++;
+                if (currentImage >= carouselImages.length) {
+                    currentImage = 0;
+                }
+                carouselImages[currentImage].style.display = 'block';
+            }, 5000);
+        } else {
+            console.error("Aucune image de classe 'carousel-image' trouvée dans la balise cible.");
+        }
+    });
 }
-    
 
 const imgColumns = document.querySelectorAll('.img-column');
 imgColumns.forEach(imgColumn => {
     const observer = new IntersectionObserver(rotation, { threshold: 0.5 });
     observer.observe(imgColumn);
 });
-
